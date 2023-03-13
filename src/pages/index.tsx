812c2d09b87dab4692d2827da3875fdc63681cd9
elmol <eliasmolini@gmail.com>
2023-03-13 19:56:16 -0300

    main component file renamed to Main

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import Head from "next/head";
 import { configureChains, createClient, WagmiConfig } from "wagmi";
 import { goerli, localhost } from "wagmi/chains";
 import { InjectedConnector } from "wagmi/connectors/injected";
-import Main from "../components/main";
+import Main from "../components/Main";
 
 const inter = Inter({ subsets: ["latin"] });
 
